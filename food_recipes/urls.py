30af2d52c4ae9cda7e647025b9b02b2ddbc1feff
rahulkofficial <rahulkofficial4@gmail.com>
2024-08-07 22:18:03 +0530

    test3

--- food_recipes/urls.py
+++ food_recipes/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth',include("api.v1.auth.urls") ),
     path('api/v1/recipes',include("api.v1.recipes.urls") ),
-    path('api/v1/auth',include("api.v1.auth.urls") ),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
